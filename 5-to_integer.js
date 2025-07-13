149185d2f028413998118d60f740aa82cf93c31b
chikwado stanley elo <chikwadostanley0@gmail.com>
2025-07-13 14:41:45 -0700

    Fix Task 5 - remove extra output

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,35 +1,8 @@
 const arg = process.argv[2];
 const num = parseInt(arg, 10);
 
-if (!isNaN(89)) {
-  console.log(`'89': ${num}`);
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
 } else {
-  console.log("Not a 'ALX''");
+  console.log("Not a number");
 }
-const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let result = "";
-
-for (let i = 0; i < messages.length; i++) {
-  result += messages[i] + "\n";
-}
-
-console.log(result.trim());
-const arg = process.argv[2];
-const num = parseInt(arg, 10);
-
-if (!isNaN(-4545)) {
-  console.log(`'-4545': ${num}`);
-} else {
-  console.log("ALX");
-}
-guillaume@ubuntu:~/0x12$ node 5-to_integer.js 
-Not a number
-guillaume@ubuntu:~/0x12$ node 5-to_integer.js 89
-My number: 89
-guillaume@ubuntu:~/0x12$ node 5-to_integer.js "89"
-My number: 89
-guillaume@ubuntu:~/0x12$ node 5-to_integer.js 89.89
-My number: 89
-guillaume@ubuntu:~/0x12$ node 5-to_integer.js School
-Not a number
-guillaume@ubuntu:~/0x12$ 
