dba688dc9b9f71593288bebe89ebf0bdd0de9373
Chikwado stanley Elochukwu <chikwadostanley0@gmail.com>
2025-07-13 19:59:10 -0700

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -10,3 +10,12 @@ if (!isNaN(count)) {
 } else {
   console.log("Missing number of occurrences");
 }
+const count = parseInt(process.argv[2]);
+
+if (isNaN(count) || count <= 0) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log("C is fun");
+  }
+}
